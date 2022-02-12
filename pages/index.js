6150272458153e886a7db8e452472bc627e3b6c3
danlooo <loosdaniel@gmail.com>
2022-02-12 19:22:59 +0000

    Update index: Use static rendering of rest api calls

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,28 @@
-import { data } from "vfile"
-import React, { useEffect, useState } from "react";
-
-const Home = () => {
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-    fetch('https://api.stackexchange.com/2.3/users/16853114?site=stackoverflow')
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setLoading(false)
-      })
-  }, [])
-
-  if (isLoading) return <p />
-  if (!data) return <p />
-
+const Home = (props) => {
   return (
     <div className="mt-3">
-      <p className="display-4 text-center">Hello!<br></br>I'm Daniel Loos @danlooo</p>
-      <p className="text-center"> {data.items[0].reputation} reputation on stack overflow</p>
+      <p className="display-4 text-center">Hello!<br></br>I am  Daniel Loos @danlooo</p>
 
+      <p className="text-center"> has {props.user.reputation} reputation and answered {props.answers}  questions on stack overflow</p>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const user_req = await fetch('https://api.stackexchange.com/2.3/users/16853114?site=stackoverflow')
+  const user_res = await user_req.json()
+
+  const answers_req = await fetch('https://api.stackexchange.com/2.2/users/16853114/answers?site=stackoverflow&filter=total')
+  const answers_res = await answers_req.json()
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      user: user_res.items[0],
+      answers: answers_res.total
+    },
+  }
 }
 
 export default Home
