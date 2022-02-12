9a6e09d816239a3c837d57f3298dfa15ca8d383b
danlooo <loosdaniel@gmail.com>
2022-02-12 18:06:48 +0000

    Use ebenezerdon/mdx-files-nextjs-blog

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+// next.config.js
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
 module.exports = {
-  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 }
---
+++ pages/bio.js
@@ -0,0 +1,9 @@
+const Bio = () => {
+  return (
+    <div className="mt-3">
+      <p className="display-4 text-center">Daniel Loos</p>
+    </div>
+  )
+}
+
+export default Bio
