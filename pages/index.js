48bf0c670711d6908c0ce603688fdbf606ead011
danlooo <loosdaniel@gmail.com>
2022-02-13 18:55:32 +0000

    Add github

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import TypingEffect from 'react-typing-effect';
 import Image from 'next/image'
-import { Button, Card, CardGroup } from 'react-bootstrap';
+import { Button, Card, CardGroup, Col, Row } from 'react-bootstrap';
 
 const Home = (props) => {
+  const orchid_url = "https://orcid.org/0000-0002-4024-4443"
+  const github_url = "https://github.com/danlooo"
+
   return (
     <div>
       <div className="my-5 text-center">
@@ -15,49 +18,76 @@ const Home = (props) => {
         </div>
       </div >
 
-      <CardGroup >
-        <Card style={{ width: '18rem' }}>
+      <CardGroup className='rowwise'>
+        <Card >
           <a href="https://stackoverflow.com/users/16853114/danlooo">
-            <Card.Img src="stackoverflow.svg" />
-            <Card.Body>
-              <Card.Title>Stack overflow contributor</Card.Title>
-              <Card.Text>
-                <p className="card-text d-flex justify-content-between">
-                  <div>{props.user?.reputation} reputation</div>
-                  <div>{props.answers} answers</div>
-                </p>
-                <p className="card-text d-flex justify-content-around ">
-                  <div><span className='gold '>● </span> {props.user.badge_counts?.gold}</div>
-                  <div><span className='silver '>● </span> {props.user.badge_counts?.silver}</div>
-                  <div><span className='bronze'>● </span> {props.user.badge_counts?.bronze}</div>
-                </p>
-              </Card.Text>
-            </Card.Body>
+            <Row>
+              <Col xs={12} sm={4} md={2} lg={2}>
+                <Card.Img src="stackoverflow.svg" />
+              </Col>
+              <Col >
+                <Card.Body>
+                  <Card.Title>Stack overflow contributor</Card.Title>
+                  <Card.Text>
+                    <p className="card-text d-flex justify-content-between">
+                      <div>{props.user?.reputation} reputation</div>
+                      <div>{props.answers} answers</div>
+                    </p>
+                    <p className="card-text d-flex justify-content-around ">
+                      <div><span className='gold '>● </span> {props.user.badge_counts?.gold}</div>
+                      <div><span className='silver '>● </span> {props.user.badge_counts?.silver}</div>
+                      <div><span className='bronze'>● </span> {props.user.badge_counts?.bronze}</div>
+                    </p>
+                  </Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
           </a>
         </Card>
 
-
-        <Card style={{ width: '18rem' }}>
-          <a href="https://orcid.org/0000-0002-4024-4443">
-            <Card.Img src="lab.png" />
-            <Card.Body>
-              <Card.Title>Scientist</Card.Title>
-              <Card.Text>
-                <p className="card-text d-flex justify-content-between">
-                  <div>3 publications</div>
-                  <div>NA citations</div>
-                </p>
-                <p>M.Sc. Bioinformatics</p>
-                <p>B.Sc. Molecular Life Science</p>
-              </Card.Text>
-            </Card.Body>
-          </a>
+        <Card >
+          <Row>
+            <Col xs={12} sm={4} md={2} lg={2}>
+              <Card.Img src="lab.png" />
+            </Col>
+            <Col>
+              <Card.Body>
+                <a href={orchid_url}><Card.Title>Scientist</Card.Title></a>                <Card.Text>
+                  <p className="card-text d-flex justify-content-between">
+                    <a href={orchid_url}>3 publications</a>
+                    <a href={orchid_url}>NA citations</a>
+                  </p>
+                  <p><a href="https://www.uni-jena.de/en/msc-bioinformatics">M.Sc. Bioinformatics, University of Jena</a></p>
+                  <p><a href="https://www.uni-luebeck.de/en/university-education/degree-programmes/molecular-life-science.html">B.Sc. Molecular Life Science, University of Lübeck</a></p>
+                </Card.Text>
+              </Card.Body>
+            </Col>
+          </Row>
         </Card>
 
+        <Card >
+          <Row>
+            <Col xs={12} sm={4} md={2} lg={2}>
+              <Card.Img src="github.svg" />
+            </Col>
+            <Col>
+              <Card.Body>
+                <a href={github_url}><Card.Title>Software engineer</Card.Title></a>
+                <Card.Text>
+                  <p className="card-text d-flex justify-content-between">
+                    <a href={github_url}>NA repositories</a>
+                    <a href={github_url}>NA  filed issues</a>
+                  </p>
+                  <p>Languages: R, Bash, </p>
+                  <p>Frameworks: R shiny, Nextflow</p>
+                </Card.Text>
+              </Card.Body>
+            </Col>
+          </Row>
+        </Card>
       </CardGroup>
 
       <p className='mt-5'>Last update: {props.date} </p>
-
     </div >
   )
 }
